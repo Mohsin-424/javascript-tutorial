1e216b91d14eb8b4cf7d75e73d86448eeda78a5b
Mohsinpu424 <mi873345@gmail.com>
2023-06-20 17:30:34 +0500

    main

---
+++ string.js
@@ -0,0 +1,20 @@
+cosnole.log('hello,world');
+let email = 'mojhsinali4241@outlook.com';
+console.log(email);
+
+
+let Firstname='Mohsin';
+
+let lastName='Ali';
+// String Cocatenatio n
+let fullname=  Firstname + ''+lastName;
+console.log(fullname);
+
+// String Length
+
+console.log(fullname[2]);
+console.log(fullname.lenght);
+
+// String Methods
+console.log(fullname.toUpperCase());
+console.log(fullname.toLowerCase());
