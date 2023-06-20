3b172de15edcb1d0e6d9807ec65c9a1259e77d8c
Mohsinpu424 <mi873345@gmail.com>
2023-06-20 17:10:07 +0500

    maijn

--- prob1.js
+++ prob1.js
@@ -3,10 +3,10 @@
 
 // Practice Steps
 // 1. Display a function with prameters time and date
-// 2.Pass parameters to derfine date andn time
+//
 // 3.Console today is tuesday(day of a month of 7days of a week)
 // 4.Current time is : 10 PM : 30 : 38(put it to console.log)
-// 5. To display the current time we will use (24 hours format)
+
 
 
 const task1= new Date();
---
+++ prob10.js
@@ -0,0 +1,29 @@
+
+// 10. Write a JavaScript program to calculate multiplication and division of two numbers (input from the user).
+// Steps
+// 1. Initalize a function that can divide and multiply both 
+// 2. give value inside a function
+
+function formultiply(a, b) {
+  return  b*a ;
+}
+
+
+a = prompt('enter value of a');
+
+b =  prompt('Enter value of b');
+formultiply(a,b);
+
+
+function fordivide(a, b) {
+  return  a/b ;
+}
+
+
+a = prompt('enter value of a');
+
+b =  prompt('Enter value of b');
+fordivide(a,b);
+
+
+
