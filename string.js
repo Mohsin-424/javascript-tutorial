167fe8f6322beac1426f5f778c81a4ee5602659d
Mohsinpu424 <mi873345@gmail.com>
2023-06-20 17:44:12 +0500

    mian

--- string.js
+++ string.js
@@ -1,20 +1,35 @@
-cosnole.log('hello,world');
-let email = 'mojhsinali4241@outlook.com';
-console.log(email);
+// cosnole.log('hello,world');
+// let email = 'mojhsinali4241@outlook.com';
+// console.log(email);
 
 
-let Firstname='Mohsin';
+// let Firstname='Mohsin';
 
-let lastName='Ali';
-// String Cocatenatio n
-let fullname=  Firstname + ''+lastName;
-console.log(fullname);
+// let lastName='Ali';
+// // String Cocatenatio n
+// let fullname=  Firstname + ''+lastName;
+// console.log(fullname);
 
-// String Length
+// // String Length
 
-console.log(fullname[2]);
-console.log(fullname.lenght);
+// console.log(fullname[2]);
+// console.log(fullname.lenght);
 
 // String Methods
 console.log(fullname.toUpperCase());
 console.log(fullname.toLowerCase());
+  
+
+
+let name= 'Muhammad Mohsin';
+let result = name.lastIndexOf('a');
+// slice will terminate the chatracteres from 0 to 5 (Muham)
+let result 1 = name.slice(0,5);
+console.log(result1);
+
+let result 2 = name.substr(0,5);
+console.log(result2);
+
+let result 3 = name.replace('Muhammad ','Ali');
+
+console.log(result3);
