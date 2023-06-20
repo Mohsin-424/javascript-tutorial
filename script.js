af029382792c346b5b0a33b9adc3e27dceea3d13
Mohsinpu424 <mi873345@gmail.com>
2023-06-20 17:23:09 +0500

    main

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 alert('Hello ,World');
 console.log(23);
 console.log('Hello ,World');
-let age =25;
-let name= 'Mohsin';
-console.log(name,age);
+// let age =25;
+// let name= 'Mohsin';
+// console.log(name,age);
+
+var name="Muahmamd ";
+age=25;
+
+console.log(name+"is"+age+"years old");
+const age = 25;
+const name = 'Mohsin';
+consnole.log(age,name);
