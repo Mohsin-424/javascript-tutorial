16e4eb05f768b7307489fcb13f71e862ff1559a8
Mohsinpu424 <mi873345@gmail.com>
2023-06-21 08:17:13 +0500

    main

---
+++ boolean.js
@@ -0,0 +1,13 @@
+let email = 'mohsinali4241@outlook.com';
+let names = ['Ali','Usaman','Mohsin'];
+console.log(email.includes('@'));
+console.log(name.includes('Usman'));
+
+// Comparisom Operators
+console.log(age==25);
+console.log(age!==25);
+console.log(age<=25);
+console.log(age>=25);
+console.log(age===25);
+console.log(age<25);
+console.log(age>25);
--- nullundefined.js
+++
@@ -1,2 +0,0 @@
-let age = null;
-console.log(age, age+3,`the age is ${age}`);
